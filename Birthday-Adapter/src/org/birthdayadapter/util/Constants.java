5f10fa929ecdb58c66b79faf249db2be8aca3c2e
Dominik Schürmann <dominik@dominikschuermann.de>
2013-02-08 19:18:45 +0100

    version 1.10

--- Birthday-Adapter/src/org/birthdayadapter/util/Constants.java
+++ Birthday-Adapter/src/org/birthdayadapter/util/Constants.java
@@ -23,7 +23,7 @@ package org.birthdayadapter.util;
 import android.accounts.Account;
 
 public class Constants {
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
     public static final String TAG = "Birthday Adapter";
 
