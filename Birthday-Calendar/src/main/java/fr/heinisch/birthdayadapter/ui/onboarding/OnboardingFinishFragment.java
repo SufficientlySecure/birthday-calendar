ceb995240ce9c019e5c30b109f429e36889ba89b
Matthias <matthias@matthiasheinisch.de>
2026-01-15 22:40:29 +0100

    Add calendar shortcut to onboarding
    Signed-off-by: Matthias <matthias@matthiasheinisch.de>

--- Birthday-Calendar/src/main/java/fr/heinisch/birthdayadapter/ui/onboarding/OnboardingFinishFragment.java
+++ Birthday-Calendar/src/main/java/fr/heinisch/birthdayadapter/ui/onboarding/OnboardingFinishFragment.java
@@ -1,15 +1,25 @@
 package fr.heinisch.birthdayadapter.ui.onboarding;
 
+import android.content.ActivityNotFoundException;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.util.Calendar;
 
 import fr.heinisch.birthdayadapter.R;
 import fr.heinisch.birthdayadapter.ui.OnboardingActivity;
@@ -19,6 +29,7 @@ public class OnboardingFinishFragment extends Fragment {
     private TextView titleTextView;
     private TextView textTextView;
     private Button ignoreButton;
+    private Button openCalendarButton;
     private boolean warningMode = false;
 
     @Nullable
@@ -28,6 +39,7 @@ public class OnboardingFinishFragment extends Fragment {
         titleTextView = view.findViewById(R.id.finish_title);
         textTextView = view.findViewById(R.id.finish_text);
         ignoreButton = view.findViewById(R.id.ignore_button);
+        openCalendarButton = view.findViewById(R.id.open_calendar_button);
 
         ignoreButton.setOnClickListener(v -> {
             if (getActivity() instanceof OnboardingActivity) {
@@ -35,8 +47,22 @@ public class OnboardingFinishFragment extends Fragment {
             }
         });
 
+        openCalendarButton.setOnClickListener(v -> openCalendar());
+
         updateTexts();
         return view;
+    }
+
+    private void openCalendar() {
+        Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+        builder.appendPath("time");
+        ContentUris.appendId(builder, Calendar.getInstance().getTimeInMillis());
+        Intent intent = new Intent(Intent.ACTION_VIEW).setData(builder.build());
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(requireActivity(), "No calendar app found", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void setWarningMode(boolean warningMode) {
@@ -51,10 +77,31 @@ public class OnboardingFinishFragment extends Fragment {
             titleTextView.setText(R.string.onboarding_finish_warning_title);
             textTextView.setText(R.string.onboarding_finish_warning_text);
             ignoreButton.setVisibility(View.VISIBLE);
+            openCalendarButton.setVisibility(View.GONE);
         } else {
             titleTextView.setText(R.string.onboarding_finish_title);
-            textTextView.setText(R.string.onboarding_finish_text);
+            setCalendarSpecificText();
             ignoreButton.setVisibility(View.GONE);
+            openCalendarButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void setCalendarSpecificText() {
+        PackageManager pm = requireActivity().getPackageManager();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_APP_CALENDAR);
+        ResolveInfo resolveInfo = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        if (resolveInfo != null) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            if ("com.google.android.calendar".equals(packageName)) {
+                textTextView.setText(R.string.onboarding_finish_text_google_calendar);
+            } else if ("com.samsung.android.calendar".equals(packageName)) {
+                textTextView.setText(R.string.onboarding_finish_text_samsung_calendar);
+            } else {
+                textTextView.setText(R.string.onboarding_finish_text);
+            }
+        } else {
+            textTextView.setText(R.string.onboarding_finish_text);
         }
     }
 }
